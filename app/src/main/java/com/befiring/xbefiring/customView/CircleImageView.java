1fef060d5db751a364079f9ac67c2c63fdfb91ea
wangmeng <392562649@qq.com>
2016-06-13 08:54:08 +0800

    增加圆形ImageView

---
+++ app/src/main/java/com/befiring/xbefiring/customView/CircleImageView.java
@@ -0,0 +1,14 @@
+package com.befiring.xbefiring.customView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Administrator on 2016/6/8.
+ */
+public class CircleImageView extends ImageView{
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
